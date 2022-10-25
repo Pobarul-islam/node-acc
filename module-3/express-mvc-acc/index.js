02adaeebe9fbc15d6bc3ff7e50e0850c33b0ffec
Pobarul-islam <pobarul3@gmail.com>
2022-10-25 20:35:38 +0600

    some code added

--- module-3/express-mvc-acc/index.js
+++ module-3/express-mvc-acc/index.js
@@ -8,14 +8,14 @@ const port = process.env.PORT || 5000;
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const nodemailer = require("nodemailer");
 const dbConnect = require("./utils/dbConnect");
-const toolsRoutes = require("./routes/tools.route");
+const toolsRoutes = require("./routes/v1/tools.route");
 
 app.use(cors());
 app.use(express.json());
 
 dbConnect();
 
-app.use("/tools", toolsRoutes)
+app.use("/api/v1/tools", toolsRoutes)
 async function run() {
   try {
     // await client.connect();
